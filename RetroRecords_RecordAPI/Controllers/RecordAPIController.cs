ccdeb81906bb990089ce104dd30066753c1e07b3
RoKen1000 <rosskennedy993@outlook.com>
2023-10-11 13:19:24 +0100

    Create RecordAPIController.

---
+++ RetroRecords_RecordAPI/Controllers/RecordAPIController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RetroRecords_RecordAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordAPIController : ControllerBase
+    {
+
+
+    }
+}
