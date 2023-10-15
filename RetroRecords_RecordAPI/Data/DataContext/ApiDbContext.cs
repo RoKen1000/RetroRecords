8d592d0cdec24764f229cbcf5f96b5637d617d2c
RoKen1000 <rosskennedy993@outlook.com>
2023-10-15 17:30:27 +0100

    Add data annotations to auto generate Id for Record model. Create ApiDbContext and inset DbSet to create Records table.

---
+++ RetroRecords_RecordAPI/Data/DataContext/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetroRecords_RecordAPI.Models;
+
+namespace RetroRecords_RecordAPI.Data.DataContext
+{
+    public class ApiDbContext : DbContext
+
+    {
+        public DbSet<Record> Records { get; set; }
+
+
+    }
+}
--- RetroRecords_RecordAPI/Models/Record.cs
+++ RetroRecords_RecordAPI/Models/Record.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RetroRecords_RecordAPI.Models
 {
     public class Record
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedAt { get; set; }
