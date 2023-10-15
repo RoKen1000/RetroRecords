f9546096e1adb320558e31547030f0962ecb2d4f
RoKen1000 <rosskennedy993@outlook.com>
2023-10-15 16:38:05 +0100

    Add additional properties to Record class and RecordDTO.

--- RetroRecords_RecordAPI/Models/Dto/RecordDTO.cs
+++ RetroRecords_RecordAPI/Models/Dto/RecordDTO.cs
@@ -14,5 +14,10 @@ namespace RetroRecords_RecordAPI.Models.Dto
         public string Artist { get; set; }
         [HiddenInput]
         public string RunTimeString { get; set; }
+        [HiddenInput]
+        public string ReleaseDateString { get; set; }
+        public string Genre { get; set; }
+        public string Country { get; set; }
+        public string Label { get; set; }
     }
 }
--- RetroRecords_RecordAPI/Models/Record.cs
+++ RetroRecords_RecordAPI/Models/Record.cs
@@ -1,10 +1,19 @@
-﻿namespace RetroRecords_RecordAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetroRecords_RecordAPI.Models
 {
     public class Record
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedAt { get; set; }
-        //public TimeSpan RunTime { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public TimeSpan RunTime { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Country { get; set; }
+        public string Label { get; set; }
+
     }
 }
