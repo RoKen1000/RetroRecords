c6581d18b7163a12f31a5d92079a47be35a730e7
RoKen1000 <rosskennedy993@outlook.com>
2023-10-17 15:32:05 +0100

    Add additional seed data into ApiDbContext and create data migrations.

--- RetroRecords_RecordAPI/Data/DataContext/ApiDbContext.cs
+++ RetroRecords_RecordAPI/Data/DataContext/ApiDbContext.cs
@@ -18,7 +18,13 @@ namespace RetroRecords_RecordAPI.Data.DataContext
         {
             modelBuilder.Entity<Record>().HasData(
                 new Record { Id = 1, Name = "Aladdin Sane", Artist = "David Bowie", CreatedAt = DateTime.Now, RunTime = new TimeSpan(0, 41, 32), Genre = "Glam Rock", ReleaseDate = new DateTime(1973, 4, 19), Label = "RCA" },
-                new Record { Id = 2, Name = "Station To Station", Artist = "David Bowie", CreatedAt = DateTime.Now, RunTime = new TimeSpan(0, 37, 54), Genre = "Rock", ReleaseDate = new DateTime(1976, 1, 23), Label = "RCA"}
+                new Record { Id = 2, Name = "Station To Station", Artist = "David Bowie", CreatedAt = DateTime.Now, RunTime = new TimeSpan(0, 37, 54), Genre = "Rock", ReleaseDate = new DateTime(1976, 1, 23), Label = "RCA"},
+                new Record { Id = 3, Name = "Purple Rain", Artist = "Prince", CreatedAt = DateTime.Now, RunTime = new TimeSpan(0, 43, 55), Genre = "Funk Pop", ReleaseDate =new DateTime(1984, 6, 25), Label = "Warner Bros."},
+                new Record { Id = 4, Name = "Selling England By The Pound", Artist = "Genesis", CreatedAt = DateTime.Now, RunTime = new TimeSpan(0, 53, 44), Genre = "Prog Rock", ReleaseDate=new DateTime(1973, 9, 28), Label = "Charisma"},
+                new Record { Id = 5, Name = "Breakfast In America", Artist = "Supertramp", CreatedAt = DateTime.Now, RunTime = new TimeSpan(0, 46, 6), Genre = "Pop", ReleaseDate = new DateTime(1979, 3, 16), Label = "A&M"},
+                new Record { Id = 6, Name = "My Arms, Your Hearse", Artist = "Opeth", CreatedAt = DateTime.Now, RunTime = new TimeSpan(0, 52, 34), Genre = "Metal", ReleaseDate = new DateTime(1998, 8, 18), Label = "Candlelight"},
+                new Record { Id = 7, Name = "Computer World", Artist = "Kraftwerk", CreatedAt = DateTime.Now, RunTime = new TimeSpan(0, 34, 25), Genre = "Electronica", ReleaseDate = new DateTime(1981, 5, 11), Label = "Kling Klang"},
+                new Record { Id = 8, Name = "Master of Puppets", Artist = "Metallica", CreatedAt = DateTime.Now, RunTime = new TimeSpan(0, 54, 52), Genre = "Metal", ReleaseDate = new DateTime(1986, 3, 3), Label = "Elektra"}
             );
         }
     }
--- RetroRecords_RecordAPI/Migrations/20231017142956_AddAdditionalSeedData.Designer.cs
+++ RetroRecords_RecordAPI/Migrations/20231017142956_AddAdditionalSeedData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RetroRecords_RecordAPI.Data.DataContext;
 
@@ -11,9 +12,11 @@ using RetroRecords_RecordAPI.Data.DataContext;
 namespace RetroRecords_RecordAPI.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231017142956_AddAdditionalSeedData")]
+    partial class AddAdditionalSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
