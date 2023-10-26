a63ccfa0d00b94125e042ab3be10bfcc2e84c76e
RoKen1000 <rosskennedy993@outlook.com>
2023-10-26 13:43:57 +0100

    Create IRepository and add in methods to repository.

---
+++ RetroRecords.Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Linq.Expressions;
+
+namespace RetroRecords.Repository.IRepository
+{
+    internal interface IRepository<Record>
+    {
+        IEnumerable<Record> GetAll();
+        Record Get(Expression<Func<Record, bool>> filter);
+        void Add(Record record);
+        void Update(Record record);
+        void Delete(Record record);
+
+    }
+}
